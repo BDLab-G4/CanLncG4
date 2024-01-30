986a5381764c41a9cf184b4f8b32c80a282c992e
= <=>
2024-01-30 21:38:00 +0530

    changed_internal_api_endpoint

--- utils/db.ts
+++ utils/db.ts
@@ -10,7 +10,7 @@ let con = {
 
 
     try {
-      const res = await axios.post('http://127.0.0.1:5000/query_' + table + '/', {
+      const res = await axios.post('http://13.232.164.101:5000/query_' + table + '/', {
         query: query
       })
 
