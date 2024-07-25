2098d992d1b9588161cd220924b87007a78db30a
Shubham Sharma <113938343+BDLab-G4@users.noreply.github.com>
2024-07-25 23:15:48 +0530

    Updated help page.tsx
    Shubham Sharma

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -79,7 +79,6 @@ const Help = () => {
                 </Box>
                 1. The navigation bar provides access to different sections of the database.
                 <br />
-                <br />
                 <Box
                   sx={{
                     position: "relative",
